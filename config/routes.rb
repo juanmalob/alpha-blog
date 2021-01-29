1974e76e65a38ab182f2b6a1bb61b5d6285a2793
juanmalob <juanmalobato@gmail.com>
2021-01-29 16:47:02 +0100

    Add user create Action

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   root "pages#home"
 
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
